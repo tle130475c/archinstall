3be0a2bf91b0d1223672a65457ac63c07f8ed245
tle130475c <tle130475c@gmail.com>
2024-09-01 17:24:55 +0700

    add vdhcoapp

--- src/main/java/com/tle130475c/archinstall/osinstall/tool/Browser.java
+++ src/main/java/com/tle130475c/archinstall/osinstall/tool/Browser.java
@@ -20,7 +20,7 @@ public class Browser implements Installable {
     @Override
     public int install() throws InterruptedException, IOException {
         installPkgs(List.of("torbrowser-launcher", "firefox-developer-edition",
-                "google-chrome", "chromium", "microsoft-edge-stable-bin"),
+                "google-chrome", "chromium", "microsoft-edge-stable-bin", "vdhcoapp"),
                 userAccount, chrootDir);
 
         return 0;
