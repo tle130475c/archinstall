1113380f459db8fdc0c86f2e47f020b74d229bb1
tle130475c <tle130475c@gmail.com>
2024-10-04 13:53:39 +0700

    add confirmation and success message for luks auto mount config

--- src/main/java/com/tle130475c/archinstall/menu/mainmenu/ConfigureLUKSAutoMount.java
+++ src/main/java/com/tle130475c/archinstall/menu/mainmenu/ConfigureLUKSAutoMount.java
@@ -1,6 +1,8 @@
 package com.tle130475c.archinstall.menu.mainmenu;
 
 import static com.tle130475c.archinstall.util.DiskUtil.configureAutoMountLUKSPartition;
+import static com.tle130475c.archinstall.util.IOUtil.confirmDefaultYes;
+import static com.tle130475c.archinstall.util.IOUtil.getConfirmation;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,18 +18,22 @@ import com.tle130475c.archinstall.util.ConfigReader;
 public class ConfigureLUKSAutoMount implements Runnable {
     @Override
     public void run() {
-        try {
-            ConfigReader configReader = new ConfigReader("install-info.xml");
-            List<LUKSPartition> partitions = configReader.getLUKSAutoMountPartitions();
-            String username = configReader.getUsername();
+        if (confirmDefaultYes(getConfirmation(":: Proceed with configuring LUKS partitions auto mount? [Y/n] "))) {
+            try {
+                ConfigReader configReader = new ConfigReader("install-info.xml");
+                List<LUKSPartition> partitions = configReader.getLUKSAutoMountPartitions();
+                String username = configReader.getUsername();
 
-            for (LUKSPartition partition : partitions) {
-                configureAutoMountLUKSPartition(partition, username);
+                for (LUKSPartition partition : partitions) {
+                    configureAutoMountLUKSPartition(partition, username);
+                }
+
+                System.console().printf("LUKS partitions auto mount configured!%n");
+            } catch (SAXException | IOException | ParserConfigurationException | XPathExpressionException e) {
+                System.console().printf("No LUKS partition information!%n");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
-        } catch (SAXException | IOException | ParserConfigurationException | XPathExpressionException e) {
-            System.console().printf("No LUKS partition information!%n");
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
         }
     }
 }
