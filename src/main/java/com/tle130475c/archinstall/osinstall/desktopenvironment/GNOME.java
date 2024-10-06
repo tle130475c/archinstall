01af73b8389abe49eb4ae11eac4504ab968dacd9
tle130475c <tle130475c@gmail.com>
2024-10-06 23:10:33 +0700

    add gnome-boxes, remove epiphany

--- src/main/java/com/tle130475c/archinstall/osinstall/desktopenvironment/GNOME.java
+++ src/main/java/com/tle130475c/archinstall/osinstall/desktopenvironment/GNOME.java
@@ -52,7 +52,7 @@ public class GNOME implements Installable {
                 "xdg-desktop-portal-gnome", "gnome-remote-desktop", "gnome-disk-utility", "libappindicator-gtk3",
                 "transmission-gtk", "power-profiles-daemon", "gvfs-smb", "gvfs-google", "gvfs-mtp", "gvfs-nfs",
                 "gnome-user-share", "gnome-logs", "evolution", "evolution-ews", "evolution-on", "gnome-software",
-                "epiphany"), chrootDir);
+                "gnome-boxes"), chrootDir);
         installPkgs(List.of(IBUS_BAMBOO_PACKAGE), userAccount, chrootDir);
 
         return 0;
