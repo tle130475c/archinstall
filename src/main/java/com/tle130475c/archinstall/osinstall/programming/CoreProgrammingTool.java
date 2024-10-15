f263dd867e9882a26a667606108f876d76dcc6d6
tle130475c <tle130475c@gmail.com>
2024-10-15 10:19:26 +0700

    add azure-cli to core programming tool

--- src/main/java/com/tle130475c/archinstall/osinstall/programming/CoreProgrammingTool.java
+++ src/main/java/com/tle130475c/archinstall/osinstall/programming/CoreProgrammingTool.java
@@ -21,8 +21,8 @@ public class CoreProgrammingTool implements Installable {
     public int install() throws InterruptedException, IOException {
         installPkgs(List.of("git", "github-cli", "kdiff3", "valgrind", "kruler", "sublime-merge", "sublime-text",
                 "visual-studio-code-bin", "postman-bin", "emacs-wayland", "gvim", "bash-language-server",
-                "kate", "dbeaver", "dbeaver-plugin-office", "dbeaver-plugin-svg-format", "powershell-bin"),
-                userAccount, chrootDir);
+                "kate", "dbeaver", "dbeaver-plugin-office", "dbeaver-plugin-svg-format", "powershell-bin",
+                "azure-cli"), userAccount, chrootDir);
 
         return 0;
     }
