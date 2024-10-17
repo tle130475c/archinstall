f8956cb62a02ff1132c9082910b00ec3f00b0445
tle130475c <tle130475c@gmail.com>
2024-10-17 11:12:25 +0700

    completely remove lombok

--- src/main/java/com/tle130475c/archinstall/partition/LUKSPartition.java
+++ src/main/java/com/tle130475c/archinstall/partition/LUKSPartition.java
@@ -1,13 +1,47 @@
 package com.tle130475c.archinstall.partition;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class LUKSPartition {
     private String partitionUUID;
     private String luksUUID;
     private String mapperName;
     private String luksPassword;
+
+    public LUKSPartition(String partitionUUID, String luksUUID, String mapperName, String luksPassword) {
+        this.partitionUUID = partitionUUID;
+        this.luksUUID = luksUUID;
+        this.mapperName = mapperName;
+        this.luksPassword = luksPassword;
+    }
+
+    public String getPartitionUUID() {
+        return partitionUUID;
+    }
+
+    public void setPartitionUUID(String partitionUUID) {
+        this.partitionUUID = partitionUUID;
+    }
+
+    public String getLuksUUID() {
+        return luksUUID;
+    }
+
+    public void setLuksUUID(String luksUUID) {
+        this.luksUUID = luksUUID;
+    }
+
+    public String getMapperName() {
+        return mapperName;
+    }
+
+    public void setMapperName(String mapperName) {
+        this.mapperName = mapperName;
+    }
+
+    public String getLuksPassword() {
+        return luksPassword;
+    }
+
+    public void setLuksPassword(String luksPassword) {
+        this.luksPassword = luksPassword;
+    }
 }
--- src/main/java/com/tle130475c/archinstall/partition/PartitionLayoutInfo.java
+++ src/main/java/com/tle130475c/archinstall/partition/PartitionLayoutInfo.java
@@ -2,13 +2,6 @@ package com.tle130475c.archinstall.partition;
 
 import com.tle130475c.archinstall.systeminfo.StorageDeviceSize;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Getter
-@Setter
 public class PartitionLayoutInfo {
     private String diskName;
     private StorageDeviceSize swapSize;
@@ -16,4 +9,62 @@ public class PartitionLayoutInfo {
     private String password;
     private Partition windowsPartition;
     private int option;
+
+    public PartitionLayoutInfo(String diskName, StorageDeviceSize swapSize, StorageDeviceSize rootSize, String password,
+            Partition windowsPartition, int option) {
+        this.diskName = diskName;
+        this.swapSize = swapSize;
+        this.rootSize = rootSize;
+        this.password = password;
+        this.windowsPartition = windowsPartition;
+        this.option = option;
+    }
+
+    public String getDiskName() {
+        return diskName;
+    }
+
+    public void setDiskName(String diskName) {
+        this.diskName = diskName;
+    }
+
+    public StorageDeviceSize getSwapSize() {
+        return swapSize;
+    }
+
+    public void setSwapSize(StorageDeviceSize swapSize) {
+        this.swapSize = swapSize;
+    }
+
+    public StorageDeviceSize getRootSize() {
+        return rootSize;
+    }
+
+    public void setRootSize(StorageDeviceSize rootSize) {
+        this.rootSize = rootSize;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Partition getWindowsPartition() {
+        return windowsPartition;
+    }
+
+    public void setWindowsPartition(Partition windowsPartition) {
+        this.windowsPartition = windowsPartition;
+    }
+
+    public int getOption() {
+        return option;
+    }
+
+    public void setOption(int option) {
+        this.option = option;
+    }
 }
