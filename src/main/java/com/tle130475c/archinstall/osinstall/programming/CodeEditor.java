f2e9604c740e55b4cfeedfb4b3c5714d8be59650
tle130475c <tle130475c@gmail.com>
2025-04-05 11:59:37 +0700

    also apply code settings when configure

--- src/main/java/com/tle130475c/archinstall/osinstall/programming/CodeEditor.java
+++ src/main/java/com/tle130475c/archinstall/osinstall/programming/CodeEditor.java
@@ -1,12 +1,16 @@
 package com.tle130475c.archinstall.osinstall.programming;
 
 import static com.tle130475c.archinstall.util.PackageUtil.installPkgs;
+import static com.tle130475c.archinstall.util.ShellUtil.getCommandRunChroot;
 import static com.tle130475c.archinstall.util.ShellUtil.getCommandRunChrootAsUser;
 import static com.tle130475c.archinstall.util.ShellUtil.runVerbose;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -30,6 +34,28 @@ public class CodeEditor implements Installable {
 
     @Override
     public int config() throws IOException, InterruptedException {
+        installExtensions();
+        copyUserSettings("/home/%s/.config/Code/User/settings.json".formatted(userAccount.getUsername()));
+        copyUserSettings("/home/%s/.config/Code - Insiders/User/settings.json".formatted(userAccount.getUsername()));
+
+        return 0;
+    }
+
+    private void copyUserSettings(String settingsPath) throws IOException, InterruptedException {
+        try (var inputStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("vscodeconfig/settings.json")) {
+            Files.copy(inputStream, Paths.get(chrootDir + settingsPath), StandardCopyOption.REPLACE_EXISTING);
+
+            // Change ownership of the copied files to the user
+            if (chrootDir != null) {
+                runVerbose(getCommandRunChroot(List.of("chown",
+                        "%s:%s".formatted(userAccount.getUsername(), userAccount.getUsername()), settingsPath),
+                        chrootDir));
+            }
+        }
+    }
+
+    private void installExtensions() {
         try (var reader = new InputStreamReader(
                 Thread.currentThread().getContextClassLoader().getResourceAsStream("vscodeconfig/extensions.txt"))) {
             Stream<String> extensions = new BufferedReader(reader).lines();
@@ -45,8 +71,6 @@ public class CodeEditor implements Installable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return 0;
     }
 
     private List<String> getExtensionInstallCommand(String extension) {
