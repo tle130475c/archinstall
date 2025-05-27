36d62cdae62ec8fe67cdc326170dde42df7687ac
tle130475c <tle130475c@gmail.com>
2025-05-27 16:11:01 +0700

    remove sublime packages

--- src/main/java/com/tle130475c/archinstall/osinstall/programming/CoreProgrammingTool.java
+++ src/main/java/com/tle130475c/archinstall/osinstall/programming/CoreProgrammingTool.java
@@ -19,7 +19,7 @@ public class CoreProgrammingTool implements Installable {
 
     @Override
     public int install() throws InterruptedException, IOException {
-        installPkgs(List.of("git", "github-cli", "kdiff3", "valgrind", "kruler", "sublime-merge", "sublime-text",
+        installPkgs(List.of("git", "github-cli", "kdiff3", "valgrind", "kruler",
                 "visual-studio-code-bin", "postman-bin", "emacs-wayland", "gvim", "bash-language-server",
                 "kate", "dbeaver", "dbeaver-plugin-office", "dbeaver-plugin-svg-format", "powershell-bin",
                 "azure-cli", "direnv", "visual-studio-code-insiders-bin", "git-lfs"), userAccount, chrootDir);
